a2e870df023d09c36df1b2c6e64312a7ee4ebc17
ahmadfajri <ahmadfajri.fajri6@gmail.com>
2020-10-06 20:24:48 +0700

    modifikasi state

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,12 @@
 import * as React from 'react'
 
 const Counter = () => {
-  let number = 0
-  const increment = () => {
-    number += 1
-    console.log(number);
-  }
+  const [number, increment] = React.useState(0)
 
   return <div>
     <h1>Counter App</h1>
     <p>Nilai counter saat ini: {number}</p>
-    <button onClick={increment}>+</button>
+    <button onClick={() => increment(number + 1)}>+</button>
   </div>
 }
 export default Counter
